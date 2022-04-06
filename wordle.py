cffd3d1ed44675fccb6ccd90f5f7c6ea7640640b
Joshy882 <joshua.mccormack.goeth@gmail.com>
2022-04-06 17:14:57 +1200

    print letter frequencies

--- wordle.py
+++ wordle.py
@@ -1,21 +1,45 @@
 # open answers for reading and create list of answers
 answers = []
-lines = 0
-with open('wordle-nyt-answers-alphabetical.txt', 'r') as reader:
-    while True:
-        line = reader.readline()
-        if line == '':
-            print('End of file')
-            break
-        else:
-            lines += 1
-            line = line.strip('\n')
-            answers.append(line)
+num_answers = 0
+with open('answers.txt', 'r') as reader:
+    line = reader.readline()
+    line = line.strip('[')
+    line = line.strip(']')
+    words = line.split(',')
+    for index in range(len(words)):
+        answers.append(words[index].strip('\"'))
+        num_answers += 1
+        
+# open allowed guesses for reading and create list of allowed guesses (incl answers)
+# answers = []
+# num_answers = 0
+# with open('answers.txt', 'r') as reader:
+#     line = reader.readline()
+#     line = line.strip('[')
+#     line = line.strip(']')
+#     words = line.split(',')
+#     for index in range(len(words)):
+#         answers.append(words[index].strip('\"'))
+#         num_answers += 1
 
-print(str(lines) + ' lines read')
-print('number of answers: ' + str(len(answers)))
-print(str(answers[10]))
-print(str(answers[100]))
-print(str(answers[1000]))
-print(str(answers[lines-1]))
-print(str(answers[-1]))
+print(str(num_answers) + ' number of answers read')
+print('number of answers in list: ' + str(len(answers)))
+
+# create letter frequency table
+alphabet = []
+for alpha in range(ord('a'), ord('z') + 1):
+    alphabet.append(chr(alpha))
+
+frequency = [0] * len(alphabet)
+for answer in answers:
+    for i in range(5):
+        letter = answer[i]
+        letter_val = ord(letter)
+        alphabet_index = letter_val - ord('a')
+        frequency[alphabet_index] += 1
+
+# print frequency of each letter in answers
+print('Letter: Frequency')
+for i in range(len(alphabet)):
+    print(alphabet[i] + '   : ' + str(frequency[i]))
+
