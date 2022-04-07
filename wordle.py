14e6254f1601c8444540ce95b42441979dac54cf
Joshy <joshua.mccormack.goeth@gmail.com>
2022-04-07 13:29:52 +1200

    write to files instead of terminal

--- wordle.py
+++ wordle.py
@@ -14,7 +14,7 @@ with open('answers.txt', 'r') as reader:
         answers.append(words[index].strip('\"'))
         num_answers += 1
 
-print(str(num_answers) + ' number of answers read')
+print('\n' + str(num_answers) + ' number of answers read')
 print('Number of answers in list: ' + str(len(answers)))
 
 # create letter frequency table
@@ -51,27 +51,30 @@ with open('allowed.txt', 'r') as reader:
     for index in range(len(words)):
         allowed.append(words[index].strip('\"'))
         num_allowed += 1
-        
-print(str(num_allowed) + ' number of allowed guesses read')
+
+print('\n' + str(num_allowed) + ' number of allowed guesses read')
 print('Number of allowed guesses in list: ' + str(len(allowed)))
 
 # combine answers and allowed lists to create master list
 master = allowed + answers
-print('Number of words in wordle list: ' + str(len(allowed)))
+print('\nTotal number of words in wordle list: ' + str(len(master)))
+
 
 ###
 # reorder alphabet based on frequency
 ###
 print('\nAlphabet sorted by frequency')
 zipped = list(zip(alphabet, frequency))
-zipped.sort(key = lambda x: x[1], reverse=True)
+zipped.sort(key=lambda x: x[1], reverse=True)
 a, f = zip(*zipped)
 for i in range(len(zipped)):
     print(str(a[i]) + ' : ' + str(f[i]))
 
-### 
+
+###
 # functions for finding words
 ###
+
 
 def find_words(word_list, num_top_letters):
     words = word_list.copy()
@@ -85,6 +88,7 @@ def find_words(word_list, num_top_letters):
             del words[j]
     return words
 
+
 def find_words_2(existing_word, search_words, num_top_letters):
     first_word = str(existing_word)
     words = search_words.copy()
@@ -97,14 +101,15 @@ def find_words_2(existing_word, search_words, num_top_letters):
             del words[j]
     return words
 
+
 def find_words_3(existing_words, search_words, num_top_letters):
     first_words = existing_words.copy()
     words = search_words.copy()
-    for j in range(len(words)-1, -1, -1): # loop through potential next words
+    for j in range(len(words)-1, -1, -1):  # loop through potential next words
         num_contained = 0
-        for i in range(num_top_letters): # loop through letters
+        for i in range(num_top_letters):  # loop through letters
             contained = False
-            for k in range(len(first_words)): # loop through existing words
+            for k in range(len(first_words)):  # loop through existing words
                 if a[i] in first_words[k]:
                     contained = True
             if not contained and a[i] in words[j]:
@@ -112,42 +117,7 @@ def find_words_3(existing_words, search_words, num_top_letters):
         if num_contained < 5:
             del words[j]
     return words
-    
 
-### 
-# Find the single best starting word
-# pick the five most common letters and look for matching word in answers
-###
-
-print('\nBest starting words that are accepted answers')
-top_six = 6
-first_words = find_words_3(['     '], answers, top_six)
-print (first_words)
-
-print('\nBest starting words that aren\'t accepted answers')
-top_five = 5
-first_words = find_words_3(['     '], master, top_five)
-print (first_words)
-
-
-### 
-# Find the two best starting words
-# pick the ten most common letters and look for matching words in answers
-###
-
-print('\nBest 2 starting words')
-top_ten = 10
-first_words = find_words_3(['     '], answers, top_ten)
-for first_word in first_words:
-    second_words = find_words_3([first_word], first_words, top_ten)
-    if len(second_words) > 0:
-        print(first_word, second_words)
-
-
-### 
-# Find the two best starting words
-# pick the ten most common letters and look for matching words in answers
-###
 
 # need to write to file because it is too long. can make it shorter by only remembering unique triplets
 def list_to_string(list_of_strings):
@@ -157,16 +127,65 @@ def list_to_string(list_of_strings):
     str1 += ']'
     return str1
 
-print('\nBest 3 starting words')
-with open('triplets.txt', 'w') as writer:
+
+###
+# Find the single best starting word
+# pick the five most common letters and look for matching word in answers
+###
+
+with open('output/singles.txt', 'w') as writer:
+    writer.write('Best starting words that are accepted answers\n')
+    top_six = 6
+    first_words = find_words_3(['     '], answers, top_six)
+    writer.write(list_to_string(first_words))
+    writer.write('\n')
+
+    writer.write('\nBest starting words that aren\'t accepted answers\n')
+    top_five = 5
+    first_words = find_words_3(['     '], master, top_five)
+    writer.write(list_to_string(first_words))
+    writer.write('\n')
+
+print('\nBest single starting words can be found in output/singles.txt')
+
+
+###
+# Find the two best starting words
+# pick the ten most common letters and look for matching words in answers
+###
+
+with open('output/pairs.txt', 'w') as writer:
+    writer.write('Best pairs of starting words\n')
+    top_ten = 10
+    first_words = find_words_3(['     '], answers, top_ten)
+    for first_word in first_words:
+        second_words = find_words_3([first_word], first_words, top_ten)
+        if len(second_words) > 0:
+            line = first_word + ' ' + list_to_string(second_words)
+            writer.write(line)
+            writer.write('\n')
+
+print('\nBest pairs of starting words can be found in output/pairs.txt')
+
+
+###
+# Find the two best starting words
+# pick the ten most common letters and look for matching words in answers
+###
+
+with open('output/triplets.txt', 'w') as writer:
+    writer.write('Best triplets of starting words\n')
     top_fifteen = 15
     first_words = find_words_3(['     '], answers, top_fifteen)
     for first_word in first_words:
         second_words = find_words_3([first_word], first_words, top_fifteen)
         for second_word in second_words:
-            third_words = find_words_3([first_word,second_word], second_words, top_fifteen)
+            third_words = find_words_3(
+                [first_word, second_word], second_words, top_fifteen)
             if len(third_words) > 0:
-                line = first_word + ' ' + second_word + ' ' + list_to_string(third_words)
-                print(line)
+                line = first_word + ' ' + second_word + \
+                    ' ' + list_to_string(third_words)
                 writer.write(line)
                 writer.write('\n')
+
+print('\nBest triplets of starting words can be found in output/triplets.txt')
